adb87caf9ae57d944658fc53425c7a051c1c89ac
Connor Johnson <Connor@Connors-MacBook-Pro-2.local>
2019-02-07 01:32:29 -0500

    fix line in tachycardia.py to comply with Pep8

--- tachycardia.py
+++ tachycardia.py
@@ -10,9 +10,9 @@ def is_tachycardic(in_word):
 
     """ User inputs a word and the script returns whether 'tachycardic' is present
 
-    :param in_word: a string argument containing a single word
+    :param in_word: string argument containing a single word
 
-    :return: a boolean value depending on presence of 'tachycardic' in the input
+    :return: boolean value depending on presence of 'tachycardic' in the input
     """
     if "tachycardic" in in_word.lower():
         tachy_out = True
